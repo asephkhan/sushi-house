f983700b1a84d0ca1c6975aee9750c0154883e55
Asif Ali <asephkhan@gmail.com>
2022-06-02 09:44:12 +0100

    styled menu card and styled contact form.

--- menu.js
+++ menu.js
@@ -1,41 +1,64 @@
 //menu buutons
-const starterBtutton = document.querySelector('.starter-button');
-const mainDishBtutton = document.querySelector('.main-dish-button');
-const dessertButton = document.querySelector('.dessert-button');
+const starterBtutton = document.querySelector(".starter-button");
+const mainDishBtutton = document.querySelector(".main-dish-button");
+const dessertButton = document.querySelector(".dessert-button");
 
 //  data
 
-const menuData = document.querySelector('.menu-container')
-const menuDataArray = Array.from(menuData.children)
+const menuData = document.querySelector(".menu-container");
+const menuDataArray = Array.from(menuData.children);
 
-const starterMenuData = menuDataArray[0]
-const mainDishMenuData = menuDataArray[1]
-const dessertMenuData = menuDataArray[2]
+ const starterMenuData = menuDataArray[0]; 
+ const mainDishMenuData = menuDataArray[1];
+ const dessertMenuData = menuDataArray[2]; 
 
 /* console.log(menuDataArray) */
 
+const hide = (elements) => {
+  elements = elements.length ? elements : [elements];
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].style.display = "none";
+  }
+};
+
+ const show = (elements) => {
+  elements = elements.length ? elements : [elements];
+  
+  for (let i = 0; i < elements.length; i++) {
+      
+    elements[i].style.display = 'flex'
+  }
+}; 
+
+// window on load show the main dishes menu
+
+window.onload = () =>{
+    
+    show(mainDishMenuData);
+    hide(starterMenuData);
+    hide(dessertMenuData);
+}
 
 // starter button event listner
 
-starterBtutton.addEventListener('click', e => {
-    starterMenuData.style.display = 'block';
-    mainDishMenuData.style.display = 'none';
-    dessertMenuData.style.display = 'none';  
-
-})
+starterBtutton.addEventListener("click", (e) => {
+  show(menuDataArray);
+  hide(mainDishMenuData);
+  hide(dessertMenuData);
+});
 
 // main-dish button event listner
 
-mainDishBtutton.addEventListener('click', e =>{
-    starterMenuData.style.display = 'none';
-    mainDishMenuData.style.display = 'block';
-    dessertMenuData.style.display = 'none';
-})
+mainDishBtutton.addEventListener("click", (e) => {
+  show(mainDishMenuData);
+  hide(starterMenuData);
+  hide(dessertMenuData);
+});
 
 // desert button event listner
 
-dessertButton.addEventListener('click', e => {
-    starterMenuData.style.display = 'none';
-    mainDishMenuData.style.display = 'none';
-    dessertMenuData.style.display = 'block'; 
-})
+dessertButton.addEventListener("click", (e) => {
+  show(dessertMenuData);
+  hide(starterMenuData);
+  hide(mainDishMenuData);
+});
